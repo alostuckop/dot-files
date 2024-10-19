5e3a937e5dee2b55a2185596b0ac4c2bbb3f2ab8
Eric Michalak <esmichalak.gmail.com>
2024-10-19 14:00:17 -0600

    use mason-null-ls to automatically install formatters and linters

--- nvim/after/plugin/lsp-zero.lua
+++ nvim/after/plugin/lsp-zero.lua
@@ -1,15 +1,32 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {'eslint'},
-    handlers = {
-        lsp_zero.default_setup,
-    },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"eslint",
+		"jedi_language_server",
+		"lua_ls",
+		"pyright",
+		"ts_ls",
+	},
+	handlers = {
+		lsp_zero.default_setup,
+	},
+})
+require("mason-null-ls").setup({
+	ensure_installed = {
+		"black",
+		"eslint_d",
+		"isort",
+		"prettier",
+		"stylua",
+		"ts-standard",
+	},
+	automatic_installation = true,
 })
