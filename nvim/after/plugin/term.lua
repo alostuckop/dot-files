30833431c6b3b4e437e451dc6bf5e285b8b2b982
Eric Michalak <esmichalak@gmail.com>
2025-12-30 18:04:55 -0700

    add terminal

---
+++ nvim/after/plugin/term.lua
@@ -0,0 +1,7 @@
+require("toggleterm").setup({
+	open_mapping = [[<leader>t]],
+	direction = "horizontal", -- 'vertical' | 'horizontal' | 'tab' | 'float'
+	float_opts = {
+		border = "curved",
+	},
+})
