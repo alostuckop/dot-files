7416347101b3bf9021d986907ad40e960430a0a8
Eric Steven Michalak <Eric Michalak>
2026-03-01 13:02:02 -0700

    update script

--- dotfiles.py
+++ dotfiles.py
@@ -2,28 +2,63 @@ import argparse
 import os
 import shutil
 
+HOME_DIR_PATH = os.path.expanduser("~/")
+
 
 def apply_config(config_name):
+    """Apply config files to local disk."""
     config_path = os.path.expanduser(f"~/.config/{config_name}")
     repo_config_path = config_name
 
     if not os.path.exists(config_path):
         os.mkdir(repo_config_path)
 
-    shutil.rmtree(config_path)
-    shutil.copytree(repo_config_path, config_path)
+    if config_name == "zsh":
+        _apply_zsh()
+    elif config_name == "bash":
+        _apply_bash()
+    else:
+        shutil.rmtree(config_path)
+        shutil.copytree(repo_config_path, config_path)
+
+
+def _apply_zsh():
+    shutil.copyfile(f"{repo_config_path}/.zshrc", f"{HOME_DIR_PATH}/.zshrc")
+    shutil.copyfile(f"{repo_config_path}/.zsh_profile", f"{HOME_DIR_PATH}/.zsh_profile")
+
+
+def _apply_bash():
+    shutil.copyfile(f"{repo_config_path}/.bashrc", f"{HOME_DIR_PATH}/.bashrc")
+    shutil.copyfile(
+        f"{repo_config_path}/.bash_profile", f"{HOME_DIR_PATH}/.bash_profile"
+    )
 
 
 def update_git(config_name):
+    """Update git repo with latest config files."""
     config_path = os.path.expanduser(f"~/.config/{config_name}")
     repo_config_path = config_name
 
     if not os.path.exists(repo_config_path):
         os.mkdir(repo_config_path)
 
-    shutil.rmtree(repo_config_path)
+    if config_name == "zsh":
+        _update_zsh(repo_config_path)
+    elif config_name == "bash":
+        _update_bash(repo_config_path)
+    else:
+        shutil.rmtree(repo_config_path)
+        shutil.copytree(config_path, repo_config_path)
 
-    shutil.copytree(config_path, repo_config_path)
+
+def _update_zsh(repo_config_path):
+    shutil.copyfile(f"{HOME_DIR_PATH}/.zshrc", repo_config_path)
+    shutil.copyfile(f"{HOME_DIR_PATH}/.zsh_profile", repo_config_path)
+
+
+def _update_bash(repo_config_path):
+    shutil.copyfile(f"{HOME_DIR_PATH}/.bashrc", repo_config_path)
+    shutil.copyfile(f"{HOME_DIR_PATH}/.bash_profile", repo_config_path)
 
 
 def clean_nvim():
